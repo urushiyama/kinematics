eee74e4b0c44c3a373634fa5decae67c8660dd8e
urushiyama <aswif10flis1ntkb@gmail.com>
2018-01-05 21:34:07 +0900

    Refactored WorldとobjectsにDashboardSceneを分割

--- src/threejs/scenes/DashboardScene.js
+++ src/threejs/scenes/DashboardScene.js
@@ -1,56 +1,16 @@
 import React, {Component} from 'react';
-import React3 from 'react-three-renderer';
 import * as THREE from 'three';
 
-import Field from '../models/Field';
-import ThreeAxisArrows from '../helpers/ThreeAxisArrows';
-
-const OrbitControls = require('three-orbit-controls')(THREE);
-
-const styles = {
-  distantPlace: {
-    color: new THREE.Color("#DDDDDD"),
-    fogDensity: 0.0125
-  }
-}
+import World from './World';
 
 class DashboardScene extends Component {
   constructor(props) {
     super(props);
-    const origin = new THREE.Vector3(0, 0, 0);
     this.state = {
-      origin: origin,
-      camera: {
-        position: new THREE.Vector3(0, 0, 5),
-        lookAt: origin,
-      },
-      arrows: {
-        origin: origin,
-        length: 10,
-        x: {
-          direction: new THREE.Vector3(1, 0, 0),
-          color: "#FF0000"
-        },
-        y: {
-          direction: new THREE.Vector3(0, 1, 0),
-          color: "#00FF00"
-        },
-        z: {
-          direction: new THREE.Vector3(0, 0, 1),
-          color: "#0000FF"
-        }
-      },
-      spotLight: {
-        position: new THREE.Vector3(0, 100, 0),
-        lookAt: origin,
-        penumbra: 0.5
-      },
       cube: {
         position: new THREE.Vector3(0, 1, 0),
         rotation: new THREE.Euler(),
-      },
-      fog: new THREE.FogExp2(styles.distantPlace.color, styles.distantPlace.fogDensity),
-      background: styles.distantPlace.color
+      }
     }
   }
 
@@ -67,64 +27,26 @@ class DashboardScene extends Component {
     });
   };
 
-  componentDidMount() {
-    const controls = new OrbitControls(this.refs.camera);
-    this.controls = controls;
-  }
-
-  componentWillUnmount() {
-    this.controls.dispose();
-    delete this.controls;
+  renderCube = () => {
+    return (
+      <mesh
+        rotation={this.state.cube.rotation}
+        position={this.state.cube.position}
+      >
+        <boxGeometry
+          width={1}
+          height={1}
+          depth={1}
+        />
+        <meshStandardMaterial
+          color={"#39ff88"}
+        />
+      </mesh>
+    );
   }
 
   render() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    return (
-      <React3
-        mainCamera="camera"
-        width={width}
-        height={height}
-        onAnimate={this.onAnimate}
-        clearColor={this.state.background}
-        pixelRatio={window.devicePixelRatio}
-      >
-        <scene fog={this.state.fog}>
-          <perspectiveCamera
-            ref="camera"
-            name="camera"
-            fov={75}
-            aspect={width / height}
-            near={0.1}
-            far={1000}
-            position={this.state.camera.position}
-            lookAt={this.state.camera.lookAt}
-          />
-          <gridHelper size={200} step={50} />
-          <axisHelper size={1000} />
-          <ThreeAxisArrows origin={this.state.origin} length={this.state.arrows.length}/>
-          <spotLight
-            penumbra={this.state.spotLight.penumbra}
-            position={this.state.spotLight.position}
-            lookAt={this.state.spotLight.lookAt}
-          />
-          <Field width={1000} height={1000} color={"#5e5e5e"}/>
-          <mesh
-            rotation={this.state.cube.rotation}
-            position={this.state.cube.position}
-          >
-            <boxGeometry
-              width={1}
-              height={1}
-              depth={1}
-            />
-            <meshStandardMaterial
-              color={"#39ff88"}
-            />
-          </mesh>
-        </scene>
-      </React3>
-    );
+    return <World onAnimate={this.onAnimate} objects={this.renderCube()}/>;
   }
 }
 
---
+++ src/threejs/scenes/World.js
@@ -0,0 +1,93 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import React3 from 'react-three-renderer';
+import * as THREE from 'three';
+
+import Field from '../models/Field';
+import ThreeAxisArrows from '../helpers/ThreeAxisArrows';
+
+const OrbitControls = require('three-orbit-controls')(THREE);
+
+const styles = {
+  distantPlace: {
+    color: new THREE.Color("#DDDDDD"),
+    fogDensity: 0.0125
+  }
+}
+
+class World extends Component {
+  constructor(props) {
+    super(props);
+    const origin = new THREE.Vector3(0, 0, 0);
+    this.state = {
+      origin: origin,
+      camera: {
+        position: new THREE.Vector3(5, 5, 5), // overriden by OrbitControls
+        lookAt: origin,
+      },
+      spotLight: {
+        position: new THREE.Vector3(0, 100, 0),
+        lookAt: origin,
+        penumbra: 0.5
+      },
+      arrows: {
+        length: 10
+      },
+      fog: new THREE.FogExp2(styles.distantPlace.color, styles.distantPlace.fogDensity),
+      background: styles.distantPlace.color
+    }
+  }
+
+  componentDidMount() {
+    const controls = new OrbitControls(this.refs.camera);
+    this.controls = controls;
+  }
+
+  componentWillUnmount() {
+    this.controls.dispose();
+    delete this.controls;
+  }
+
+  render() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const arrowsLength = (this.controls !== undefined)
+      ? this.controls.object.position.distanceTo(this.state.origin) / 5
+      : 1;
+    return (
+      <React3
+        mainCamera="camera"
+        width={width}
+        height={height}
+        onAnimate={this.props.onAnimate}
+        clearColor={this.state.background}
+        pixelRatio={window.devicePixelRatio}
+      >
+        <scene fog={this.state.fog}>
+          <perspectiveCamera
+            ref="camera"
+            name="camera"
+            fov={75}
+            aspect={width / height}
+            near={0.1}
+            far={1000}
+            {...this.state.camera}
+          />
+          <gridHelper size={200} step={50} />
+          <axisHelper size={1000} />
+          <ThreeAxisArrows origin={this.state.origin} length={arrowsLength} />
+          <spotLight {...this.state.spotLight} />
+          <Field width={1000} height={1000} color={"#5e5e5e"} />
+          {this.props.objects}
+        </scene>
+      </React3>
+    );
+  }
+}
+
+World.propTypes = {
+  onAnimate: PropTypes.func,
+  objects: PropTypes.element.isRequired
+}
+
+export default World;
