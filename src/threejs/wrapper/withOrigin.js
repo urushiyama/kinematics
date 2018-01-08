e90657f1798d1078823c62f04583749a82ec7b6c
urushiyama <aswif10flis1ntkb@gmail.com>
2018-01-08 14:40:44 +0900

    Fixed D-H記法の関節の座標表示を修正

--- src/threejs/wrapper/withOrigin.js
+++ src/threejs/wrapper/withOrigin.js
@@ -5,10 +5,16 @@ import ThreeAxisArrows from '../helpers/ThreeAxisArrows';
 export const withOrigin = InnerComponent => {
   return class extends Component {
     render() {
+      let innerProps = Object.assign({}, this.props);
+      delete innerProps.children;
       return (
         <group position={this.props.origin.position} rotation={this.props.origin.rotation}>
-          <ThreeAxisArrows length={20} visible={this.props.visibleAxisArrows}/>
-          <InnerComponent {...this.props}/>
+          <InnerComponent {...innerProps}>
+            <group>
+              <ThreeAxisArrows length={20} visible={this.props.visibleAxisArrows}/>
+              {this.props.children}
+            </group>
+          </InnerComponent>
         </group>
       );
     }
